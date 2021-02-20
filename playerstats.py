3e78a2c5352f26ba33a645fbcc468f609eaa0358
ThomasBaskin <tomcbaskin@gmail.com>
2021-02-20 14:17:19 -0700

    turned data into dataframe

--- playerstats.py
+++ playerstats.py
@@ -5,21 +5,28 @@ import numpy as np
 
 from bs4 import BeautifulSoup 
 import requests
-
-def getStats():
-    page = requests.get("https://www.basketball-reference.com/players/j/jamesle01.html")
-    soup = BeautifulSoup(page.content,'html.parser')
-    all_per_game = soup.find("div",{"id":"all_per_game"})
-    all_per_game = soup.find("div",{"id":"div_per_game"})
-    #getColumnHeads = soup.find("thead").find("tr")
-    for th in soup.findAll("th",scope="col"):
-        print(th.text) #each th.text is the col header
-        #print(th.get("aria-label"))
-    all_rows = all_per_game.find("tbody")
-    print("-----------")
-    print(all_rows)
-    
-
-if __name__ == "__main__":
-    getStats()
+page = requests.get("https://www.basketball-reference.com/players/j/jamesle01.html")
+soup = BeautifulSoup(page.content,'html.parser')
+all_per_game = soup.find("div",{"id":"all_per_game"})
+all_per_game = soup.find("div",{"id":"div_per_game"})
+columns = []
+for th in soup.findAll("th",scope="col"):
+    columns.append(th.text)
+    #print(th.text) #each th.text is the col header
+    #print(th.get("aria-label"))
+all_rows = all_per_game.find("tbody")
+print("-----------")
+data = []
+for td in all_rows:
+    try:
+        list1 = []
+        for j in td:
+            list1.append(j.text)
+        data.append(list1)
+    except:
+        print ("------")
+print(data)
+# %%
+df = pd.DataFrame(data, columns=columns) 
+display(df.head())
 # %%
